499cde638f0374ca28627f7022e849b01ba01e6b
octomonoli1 <octavio.mo90@gmail.com>
2025-02-23 20:00:46 +0100

    config cors

--- src/main/java/com/personal/octavio/controller/ProjectController.java
+++ src/main/java/com/personal/octavio/controller/ProjectController.java
@@ -4,16 +4,14 @@ import com.personal.octavio.model.Project;
 import com.personal.octavio.service.ProjectService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/project")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProjectController {
 
     private ProjectService projectService;
